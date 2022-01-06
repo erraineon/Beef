1e822d735f9d7c76e137f4df9ba7035878b61ad3
Errai <erraineon@gmail.com>
2022-01-06 01:01:51 -0500

    Add migrations

--- Beef/Program.cs
+++ Beef/Program.cs
@@ -11,35 +11,71 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+CreateHostBuilder(args)
+    .Build()
+    .Run();
 
-Host.CreateDefaultBuilder(args)
-    .ConfigureServices(
-        (hostBuilderContext, services) =>
-        {
-            var typesToExclude = new List<Type>
+static IHostBuilder CreateHostBuilder(string[] strings)
+{
+    return Host.CreateDefaultBuilder(strings)
+        .ConfigureServices(
+            (hostBuilderContext, services) =>
             {
-                typeof(BeefDbContext), typeof(ChatContext), typeof(BotInteraction), typeof(BotInteractionData),
-                typeof(BotInteractionDataOption)
-            };
+                var typesToExclude = new List<Type>
+                {
+                    typeof(BeefDbContext), typeof(ChatContext), typeof(BotInteraction), typeof(BotInteractionData),
+                    typeof(BotInteractionDataOption)
+                };
 
-            services
-                .AddDbContext<IBeefDbContext, BeefDbContext>(
-                    optionsBuilder => optionsBuilder.UseSqlite(
-                        hostBuilderContext.Configuration.GetConnectionString(nameof(BeefDbContext))
-                    )
+                services
+                    .AddDbContext<IBeefDbContext, BeefDbContext>(
+                        optionsBuilder => optionsBuilder.UseSqlite(
+                            hostBuilderContext.Configuration.GetConnectionString(nameof(BeefDbContext))
+                        )
+                    );
+
+                services.AddScoped<IChatContext, ChatContext>();
+                //services.AddScoped(s => s.GetRequiredService<IChatContext>().ChatClient);
+                //services.AddScoped(s => s.GetRequiredService<IChatContext>().CommandRegistrar);
+
+
+                services.Scan(
+                    x => x.FromAssemblyOf<ChatStartupService>()
+                        .AddClasses(
+                            f => f
+                                // Don't register the modules.
+                                .NotInNamespaceOf<TestModule>()
+                                .Where(t => !typesToExclude.Contains(t))
+                                .Where(ExcludeConcreteTypesAndRecords)
+                        )
+                        .AsImplementedInterfaces()
+                        .WithTransientLifetime()
                 );
 
-            services.AddScoped<IChatContext, ChatContext>();
-            //services.AddScoped(s => s.GetRequiredService<IChatContext>().ChatClient);
-            //services.AddScoped(s => s.GetRequiredService<IChatContext>().CommandRegistrar);
+                AddDiscord(services, hostBuilderContext);
+            }
+        );
 
+    void AddDiscord(
+        IServiceCollection services,
+        HostBuilderContext hostBuilderContext
+    )
+    {
+        var discordOptions = hostBuilderContext.Configuration.GetFromSection<DiscordOptions>();
+        if (discordOptions != null)
+        {
+            var singletonTypes = new[] {typeof(DiscordSocketClientWrapper), typeof(InteractionServiceWrapper)};
+            var typesToExclude = new List<Type> {typeof(DiscordOptions)};
+
+            services.AddSingleton<IDiscordOptions>(discordOptions);
+            if (!hostBuilderContext.HostingEnvironment.IsProduction())
+                typesToExclude.Add(typeof(DiscordGlobalCommandRegistrationService));
 
             services.Scan(
-                x => x.FromAssemblyOf<ChatStartupService>()
+                x => x.FromAssemblyOf<DiscordOptions>()
                     .AddClasses(
                         f => f
-                            // Don't register the modules.
-                            .NotInNamespaceOf<TestModule>()
+                            .Where(t => !singletonTypes.Contains(t))
                             .Where(t => !typesToExclude.Contains(t))
                             .Where(ExcludeConcreteTypesAndRecords)
                     )
@@ -47,45 +83,14 @@ Host.CreateDefaultBuilder(args)
                     .WithTransientLifetime()
             );
 
-            AddDiscord(services, hostBuilderContext);
+            services.Scan(
+                x => x.AddTypes(singletonTypes)
+                    .AsSelfWithInterfaces()
+                    .WithSingletonLifetime()
+            );
         }
-    )
-    .Build()
-    .Run();
-
-void AddDiscord(
-    IServiceCollection services,
-    HostBuilderContext hostBuilderContext
-)
-{
-    var discordOptions = hostBuilderContext.Configuration.GetFromSection<DiscordOptions>();
-    if (discordOptions != null)
-    {
-        var singletonTypes = new[] { typeof(DiscordSocketClientWrapper), typeof(InteractionServiceWrapper) };
-        var typesToExclude = new List<Type> { typeof(DiscordOptions) };
-
-        services.AddSingleton<IDiscordOptions>(discordOptions);
-        if (!hostBuilderContext.HostingEnvironment.IsProduction())
-            typesToExclude.Add(typeof(DiscordGlobalCommandRegistrationService));
-
-        services.Scan(
-            x => x.FromAssemblyOf<DiscordOptions>()
-                .AddClasses(
-                    f => f
-                        .Where(t => !singletonTypes.Contains(t))
-                        .Where(t => !typesToExclude.Contains(t))
-                        .Where(ExcludeConcreteTypesAndRecords)
-                )
-                .AsImplementedInterfaces()
-                .WithTransientLifetime()
-        );
-
-        services.Scan(
-            x => x.AddTypes(singletonTypes)
-                .AsSelfWithInterfaces()
-                .WithSingletonLifetime()
-        );
     }
-}
 
-bool ExcludeConcreteTypesAndRecords(Type t) => t.GetInterfaces().Any() && t.GetMethods().All(m => m.Name != "<Clone>$");
+    bool ExcludeConcreteTypesAndRecords(Type t) =>
+        t.GetInterfaces().Any() && t.GetMethods().All(m => m.Name != "<Clone>$");
+}
